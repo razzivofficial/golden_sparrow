aeb7c4b7715c3c9a73afc2aa9588cc48668f2466
Diptiranjan <iamdiptiranjan2004@gmail.com>
2023-07-28 21:21:19 +0530

    Backend Code Added and razorpay integrated

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import AllRoutes from "./AllRoute";
+import Details from "./pages/details/Details";
+import Invoice from "./pages/invoice/Invoice";
+import SignUp from "./pages/signup/Signup";
+import AddToCart from "./pages/addToCart/addCart";
+import PCart from "./components/cart/cart";
+import Cart from "./components/cart/cart";
+
+function App() {
+  return (
+    <>
+      <AllRoutes />
+      {/* <Details /> */}
+      {/* <Invoice /> */}
+      {/* <SignUp /> */}
+      {/* <AddToCart /> */}
+      {/* <PCart/> */}
+      {/* <AddToCart/> */}
+      {/* <Cart/> */}
+    </>
+  );
+}
+
+export default App;
