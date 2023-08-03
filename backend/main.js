edf1ef90f836128eac5c060b462a0929f1b53c5a
razzivofficial <razzivofficial@gmail.com>
2023-08-03 12:22:01 +0530

    Cleaning main

--- backend/main.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connection = mongoose.connect(process.env.MONGO_URL, {
-  dbName: "goldensparrow",
-});
-
-module.exports = { connection };
